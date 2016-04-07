26ce9bd388e7a2834b60d3130a2b79a73081dd7a
ototadana <ototadana@gmail.com>
2016-04-08 07:55:46 +0900

    update.sh にログ加工処理を追加

--- bin/update.sh
+++ bin/update.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
 set -e
 
-cd $(dirname $0)
+BASE_DIR=$(cd $(dirname $0); pwd)
+LOG_DIR=${BASE_DIR}/log
 
+execute() {
+    LOG_FILE=${LOG_DIR}/$1.log
+    echo "### $1"
+    ./$1.sh > ${LOG_FILE} 2>&1
+
+    if [ "$2" = "print_log" ]; then
+        cat ${LOG_FILE}
+        echo ""
+    fi
+}
+
+on_exit() {
+    if [ "$?" -ne 0 ]; then
+        tail -100 ${LOG_FILE}
+        echo ""
+        if [ `wc -l ${LOG_FILE} | cut -d' ' -f1` -gt 100 ]; then
+            echo "See ${LOG_FILE}"
+            echo ""
+        fi
+        echo "Error!"
+    fi
+}
+
+cd ${BASE_DIR}
 ./check-wip.sh
-./update-images.sh
-./build-images.sh
-./build-images-on-docker-hub.sh
-./update-pocci.sh
-./pull-new-images.sh
-./test-pocci.sh
+
+if [ -d ${LOG_DIR} ]; then
+    rm -fr ${LOG_DIR}
+fi
+mkdir ${LOG_DIR}
+
+trap "on_exit" EXIT
+execute update-images print_log
+execute build-images
+execute build-images-on-docker-hub
+execute update-pocci print_log
+execute pull-new-images
+execute test-pocci
+echo ""
+echo "Done."
