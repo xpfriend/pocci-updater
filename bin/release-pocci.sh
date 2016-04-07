85c96c8828c74bb9413d5f240b755ebbc8f77870
ototadana <ototadana@gmail.com>
2016-04-07 10:57:19 +0900

    release-pocci にチェック機能を追加

--- bin/release-pocci.sh
+++ bin/release-pocci.sh
@@ -7,12 +7,20 @@ POCCI_BASE_DIR=${BASE_DIR}/pocci/src.tmp
 source ${BASE_DIR}/util.sh
 
 cd ${POCCI_BASE_DIR}
-if [ `get_number_of_diff_lines` -eq 0 ]; then
-    exit
+if [ `git status --porcelain |wc -l` -gt 0 ]; then
+    echo "Uncommitted changes:"
+    git status --porcelain | sed "s/^/    /g"
+    exit 1
 fi
 
-if [ `git log --oneline master..wip | wc -l` -gt 1 ]; then
-    echo "Too many commits"
+if [ `get_number_of_diff_lines` -eq 0 ]; then
+    echo "Nothing to do"
+    exit 1
+fi
+
+NUM_COMMITS=`git log --oneline master..wip | wc -l | sed 's/ //g'`
+if [ "${NUM_COMMITS}" -gt 1 ]; then
+    echo "Too many commits (${NUM_COMMITS})"
     exit 1
 fi
 
@@ -24,4 +32,4 @@ CURRENT_VERSION=`git describe --tags --abbrev=0 | tr -d v`
 NEXT_VERSION=`get_next_version ${CURRENT_VERSION} p`
 git tag "v${NEXT_VERSION}"
 git push origin master
-git push origin "v${TAG}"
+git push origin "v${NEXT_VERSION}"
