1100f8d5f20d44334317247d77483690f23a398e
ototadana <ototadana@gmail.com>
2016-04-08 11:50:58 +0900

    pull-new-images の中に起動中コンテナの削除処理を追加

--- bin/pull-new-images.sh
+++ bin/pull-new-images.sh
@@ -4,6 +4,9 @@ set -e
 BASE_DIR=$(cd $(dirname $0); pwd)
 NEW_IMAGES=${BASE_DIR}/new-images.txt
 
+
+${BASE_DIR}/pocci/src.tmp/test/clean-containers.sh
+
 for i in `cat ${NEW_IMAGES} | sort | uniq`; do
     NAME=`echo $i | cut -d: -f1`
     IMAGES=`docker images |grep ${NAME} |awk '{printf "%s:%s ",$1,$2}'`
